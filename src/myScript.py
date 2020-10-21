a5103856bf60842bc608046fac551ca5b0d1eb95
suraj vishwakarma <34504675+vsuraj3@users.noreply.github.com>
2020-10-20 23:08:36 -0400

    Fix : updated the code

--- src/myScript.py
+++ src/myScript.py
@@ -1,4 +1,4 @@
-pystrich.datamatrix import DataMatrixEncoder
+from pystrich.datamatrix import DataMatrixEncoder
 
 encoder = DataMatrixEncoder('This is a DataMatrix.')
 encoder.save('./datamatrix_test.png')
